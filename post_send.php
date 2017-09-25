014057f5719bbb98b4b951b93bd3fc5181de2f45
conpliance2 <dxdgamjt@gmail.com>
2017-09-25 12:09:11 +0900

    送信画面編集

--- post_send.php
+++ post_send.php
@@ -15,5 +15,20 @@
 		<div class="menu_body"><a href="#">アドレス一覧</a></div>
 		<div class="menu_body"><a href="#">メール作成</a></div>
 	</div>
+		<div id="main_edit">
+	<h1>新規投稿</h1>
+	<form action="postnew_send.php" method="post" name="form1" onSubmit = "return check()" enctype="multipart/form-data">
+	<div class="koumoku-box">
+	<h2>タイトル[必須]</h2>
+	<input type="text" name="text" size="50" value="">
+	</div>
+	<div class="koumoku-box">
+	<h2>本文[必須]</h2>
+	<textarea name="textarea" cols="100" rows="50" required=""></textarea>
+	</div>
+	<input type="submit" value="確定" class="hensyu_btn">
+	</form>
+	</div>
+</div>
 </body>
 </html>
