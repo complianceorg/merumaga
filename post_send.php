48e6ecb6f077018b1c4e048f9b5d0a3ad671e4be
conpliance2 <dxdgamjt@gmail.com>
2017-09-25 12:00:59 +0900

    first commit

---
+++ post_send.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>メール送信</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="high" class="clearfix">
+		<div class="welcome_msg">管理画面</div>
+		<div class="logout_btn"><a href="logout.php">ログアウト</a></div>
+	</div>
+	<div id="side">
+		<div class="menu_body"><a href="#">アドレス一覧</a></div>
+		<div class="menu_body"><a href="#">メール作成</a></div>
+	</div>
+</body>
+</html>
