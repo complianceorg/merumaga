647621e875d42af2eaee88bd793fd5df6b368bc2
compliance1 <y_koga@compliance-co.jp>
2017-09-26 13:23:31 +0900

    completed edit and send

--- index.php
+++ index.php
@@ -1,43 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>管理画面</title>
-	<link rel="stylesheet" href="style.css">
-	<?php
-	require_once(__DIR__ . '/../config/config.php');
-	require_once(__DIR__ . '/../lib/functions.php');
+  <?php
+	include_once(__DIR__ . '/default/default.php');
 	$app = new MyApp\Controller\Index();
 	$app->run();
- ?>
-</head>
-<body>
-	<div id="high" class="clearfix">
-		<div class="welcome_msg">管理画面</div>
-		<div class="logout_btn">
-	    <form action="logout.php" method="post" id="logout">
-      <?= h($app->me()->email); ?> <input type="submit" value="ログアウト">
-      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
-    </form>
- </div>
-	</div>
-	<div id="side">
-		<div class="menu_body"><a href="#">アドレス一覧</a></div>
-		<div class="menu_body"><a href= "post_send.php" >メール作成</a></div>
-	</div>
+	?>
 	<h1>メール一覧</h1>
 	<div class="kizi-box">
-	<a href="#" class="list">
-	<ul class="kizi-list-box">
-	<li class="kizi-list"></li>
-	<li class="kizi-list"></li>
-	</ul>
-	</a>
-	<form id="send" action="" method="post">
-		<input type="number" value="">
-        <div class="sendbtn" onclick="document.getElementById('send').submit();">メールを送る</div>
-	</form>
+		<?php foreach ($app->getValues()->posts as $post) : ?>
+		<ul class="kizi-list-box">
+			<li class="kizi-list"><?= h($post->title);?></li>
+			<li class="kizi-list"><?= h($post->contain); ?></li>
+		</ul>
+		<a href="<?= SITE_URL.'edit.php?id='. h($post->id); ?>" class="list">詳細</a>
+		<form id="send<?= h($post->id); ?>" action="" method="post">
+			<input type="hidden" value="<?= h($post->id); ?>" name="id">
+	    <div class="sendbtn" onclick="document.getElementById('send<?= h($post->id); ?>').submit();">メールを送る</div>
+			<input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+		</form>
+	  <?php endforeach; ?>
 	</div>
 </body>
 </html>
--- post_send.php
+++ post_send.php
@@ -1,33 +1,8 @@
 <?php
-require_once(__DIR__ . '/../config/config.php');
-require_once(__DIR__ . '/../lib/functions.php');
-//$ip = $_SERVER["REMOTE_ADDR"];
-//ip_check($ip);
-
-$app = new MyApp\Controller\Post();
-$app->run();
+  require_once(__DIR__ . '/default/default.php');
+	$app = new MyApp\Controller\Post();
+	$app->run();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>メール送信</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<div id="high" class="clearfix">
-		<div class="welcome_msg">管理画面</div>
-		<div class="logout_btn">	    <form action="logout.php" method="post" id="logout">
-		      <?= h($app->me()->email); ?> <input type="submit" value="ログアウト">
-		      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
-		    </form>
-  </div>
-	</div>
-	<div id="side">
-		<div class="menu_body"><a href="#">アドレス一覧</a></div>
-		<div class="menu_body"><a href="post_send.php">メール作成</a></div>
-	</div>
 	<div id="main_edit">
 			<?php $app->ok(); ?>
 	<h1>新規投稿</h1>
