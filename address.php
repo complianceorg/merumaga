d5e090f613e5af20a50ed7a2b0873e79f3015194
compliance1 <y_koga@compliance-co.jp>
2017-09-27 11:02:53 +0900

    upgrade address list

--- address.php
+++ address.php
@@ -6,11 +6,22 @@
   <div id="main_edit">
 	<h1>アドレス一覧</h1>
 	<div class="kizi-box">
+    <p>ドメイン/public/login/signupから</p>
+    <select name="" class="kizi-list-box">
 		<?php foreach ($app->getValues()->addresses as $address) : ?>
-		<ul class="kizi-list-box">
-			<li class="kizi-list"><?= h($address->email);?></li>
-		</ul>
+			<option><?= h($address->email);?></option>
 	  <?php endforeach; ?>
+  　</select>
+  　<span>(<?= count($app->getValues()->addresses); ?>)</span>
+
+    <p>その他から</p>
+    <select name="" class="kizi-list-box">
+    <?php foreach ($app->getValues()->addresses_post as $address) : ?>
+      <option><?= h($address->email);?></option>
+    <?php endforeach; ?>
+  　</select>
+  　<span>(<?= count($app->getValues()->addresses_post); ?>)</span>
+
 	</div>
 </div>
 </body>
